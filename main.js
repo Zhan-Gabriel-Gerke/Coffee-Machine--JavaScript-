f192fc1dd731c51cf876c0b2e2c851e4ab0c82df
Zhan-Gabriel Gerke <93312041+Zhan-Gabriel-Gerke@users.noreply.github.com>
2025-11-20 13:30:16 +0200

    Stage4

--- main.js
+++ main.js
@@ -3,51 +3,81 @@
 // You will need this in the following stages
 const input = require('sync-input')
 
-// console.log("Starting to make a coffee\n" +
-//     "Grinding coffee beans\n" +
-//     "Boiling water\n" +
-//     "Mixing boiled water with crushed coffee beans\n" +
-//     "Pouring coffee into the cup\n" +
-//     "Pouring some milk into the cup\n" +
-//     "Coffee is ready!")
+let coffee = 120
+let milk = 540
+let water = 400
+let cups = 9
+let money = 550
 
-// console.log("Write how many cups of coffee you will need:")
-//
-// cupsOfCoffee = input()
-//
-// console.log(`For ${cupsOfCoffee} cups of coffee you will need:
-// ${200 * cupsOfCoffee} ml of water
-// ${50 * cupsOfCoffee} ml of milk
-// ${15 * cupsOfCoffee} g of coffee beans`)
+function buyCoffee(){
 
-function cupsOfCoffee (milk, water, coffee){
-    let waterRequiredForOneCoffee = 200
-    let milkRequiredForOneCoffee = 50
-    let coffeeRequiredForOneCoffee = 15
+    const waterForEspresso = 250, coffeeForEspresso = 16
+    const waterForLatte = 350, milkForLatte = 75, coffeeForLatte = 20
+    const waterForCapucino = 200, milkForCapucino = 100, coffeeForCapucino = 12
 
-    let arrayOfCoffee = new Array(3);
+    console.log("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+    let answer = input()
 
-    arrayOfCoffee[0] = milk / milkRequiredForOneCoffee
-    arrayOfCoffee[1] = water / waterRequiredForOneCoffee
-    arrayOfCoffee[2] = coffee / coffeeRequiredForOneCoffee
-
-
-    return Math.floor(Math.min(arrayOfCoffee[0], arrayOfCoffee[1], arrayOfCoffee[2]))
+    if (answer == 1){
+        if (water > waterForEspresso && coffee > coffeeForEspresso){
+            water = water - waterForEspresso
+            coffee = coffee - coffeeForEspresso
+            cups--
+            money = money + 4
+        }
+    }else if (answer == 2){
+        if (water > waterForLatte && milk > milkForLatte && coffee > coffeeForLatte){
+            water = water - waterForLatte
+            coffee = coffee - coffeeForLatte
+            milk = milk - milkForLatte
+            cups--
+            money = money + 7
+        }
+    }else if (answer == 3){
+        if (water > waterForCapucino && milk > milkForCapucino && coffee > coffeeForCapucino){
+            water = water - waterForCapucino
+            milk = milk - milkForCapucino
+            coffee = coffee - coffeeForCapucino
+            cups--
+            money = money + 6
+        }
+    }else {
+        console.log("Error")
+    }
 
 }
 
-let amountOfTheWater = Number(input("Write how many ml of water the coffee machine has:"))
-let amountOfTheMilk = Number(input("Write how many ml of milk the coffee machine has:"))
-let amountOfTheCoffee = Number(input("Write how many grams of coffee beans the coffee machine has:"))
-let cupsOfCoffeeNeedToDo = Number(input("Write how many cups of coffee you will need:"))
+function addStuff() {
+    water = water + Number(input("Write how many ml of water you want to add:"))
+    milk = milk + Number(input("Write how many ml of milk you want to add:"))
+    coffee = coffee + Number(input("Write how many grams of coffee beans you want to add:"))
+    cups = cups + Number(input("Write how many disposable cups you want to add:"))
+}
+
+function showAmount(){
+    console.log(`The coffee machine has:\n` +
+        `${water} ml of water\n` +
+        `${milk} ml of milk\n` +
+        `${coffee} g of coffee beans\n` +
+        `${cups} disposable cups\n` +
+        `$${money} of money`)
+}
+
+function takeMoney(){
+    console.log(`I gave you ${money}`)
+    money = 0
+}
 
 
-let canMakeCups = cupsOfCoffee(amountOfTheMilk, amountOfTheWater, amountOfTheCoffee)
-
-if (cupsOfCoffeeNeedToDo < canMakeCups) {
-    console.log(`Yes, I can make that amount of coffee (and even ${canMakeCups - cupsOfCoffeeNeedToDo} more than that)`)
-} else if (canMakeCups < cupsOfCoffeeNeedToDo){
-    console.log(`No, I can make only ${canMakeCups} cups of coffee`)
-} else {
-    console.log("Yes, I can make that amount of coffee")
+while (true){
+    showAmount()
+    console.log("Write action (buy, fill, take): ")
+    let answer = input()
+    if (answer == "buy"){
+        buyCoffee()
+    }else if (answer == "fill"){
+        addStuff()
+    }else if (answer == "take"){
+        takeMoney()
+    }
 }
